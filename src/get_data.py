55826bb7797db1cd87c2c1aa1f4f9927b8dabf2d
Roman Panibratyuk <panibrat@usc.edu>
2024-04-09 12:20:27 -0700

    attempting to extract data from html table in webpage

--- src/get_data.py
+++ src/get_data.py
@@ -4,22 +4,22 @@ import csv
 from bs4 import BeautifulSoup
 from io import StringIO
 
-flu_2022_df = pd.read_csv("/Users/macbook/DS510/final_project/data/raw/Influenza_Vaccination_Coverage_for_All_Ages__6__Months_.csv")
-#print(flu_2022_df.head())
-# I'm choosing the range of the Flu Vaccination between 2022-2023
-flu_2022_23 = flu_2022_df[flu_2022_df['Season/Survey Year'] == '2022-23']
-flu_2022_23['Estimate (%)'] = pd.to_numeric(flu_2022_23['Estimate (%)'], errors='coerce')
+# flu_2022_df = pd.read_csv("/Users/macbook/DS510/final_project/data/raw/Influenza_Vaccination_Coverage_for_All_Ages__6__Months_.csv")
+# print(flu_2022_df.head())
+# # I'm choosing the range of the Flu Vaccination between 2022-2023
+# flu_2022_23 = flu_2022_df[flu_2022_df['Season/Survey Year'] == '2022-23']
+#flu_2022_23.loc[:,'Estimate (%)'] = pd.to_numeric(flu_2022_23['Estimate (%)'], errors='coerce')
 #I'm trying to find the average precentage of people vaccinated between the years 2022-2023 
-average_percentage = flu_2022_23['Estimate (%)'].mean()
+# average_percentage = flu_2022_23['Estimate (%)'].mean()
 #print(average_percentage)
-#retriving data from the cdc website and get html response to get CSV data using beautifulsoup
-url = "https://covidtracking.com/data/api"
-response  = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-csv_link = soup.find("a",href = True, text = "https://api.covidtracking.com/v1/states/daily.csv")
-if csv_link:
-    csv_url = csv_link["href"]
-    csv_response = requests.get(csv_url)
+#retriving data from the cdc website and get html "response to get CSV data using beautifulsoup
+# url = "https://covid.cdc.gov/covid-data-tracker/#vaccinations_vacc-people-booster-percent-pop5"
+# response  = requests.get(url)
+# soup = BeautifulSoup(response.content, 'html.parser')
+# csv_link = soup.find("button",href = True, text = " Download Data ")
+# if csv_link:
+#     csv_url = csv_link["href"]
+#     csv_response = requests.get(csv_url)
     #print(csv_response.text)
     # csv_data_covid = csv_response.text
     # rows = csv_data_covid.split("\n")
@@ -28,9 +28,10 @@ if csv_link:
     #     for column in columns:
     #         print(column)
     #     print()
-    csv_data_covid = csv_response.content.decode("utf-8")
-    covid_df = pd.read_csv(StringIO(csv_data_covid))
-    print(covid_df)
+    # csv_data_covid = csv_response.content.decode("utf-8")
+    # covid_df = pd.read_csv(StringIO(csv_data_covid))
+    # print(covid_df.head())
+    
 
     
 
@@ -43,3 +44,38 @@ if csv_link:
 #         print(row)
 # #else:
 #     print("CSV data not found.")
+
+
+#scrapping data using table
+def scrape_data(url):
+    
+    response  = requests.get(url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        #print(soup.prettify())
+        table = soup.find('table')
+        
+        if table:
+            data = []
+            rows = table.find_all('tr')
+            for row in rows:
+                cols = row.find_all(['th', 'td'])
+                cols = [col.text.strip() for col in cols]
+                data.append(cols)
+            return data
+        
+        else:
+            print('error no table with class name')
+            return None
+            
+    else:
+        print('error failed to retrive data')
+        return None
+    
+url = 'https://covid.cdc.gov/covid-data-tracker/#vaccinations_vacc-total-admin-count-total'
+scraped_data = scrape_data(url)
+
+if scraped_data:
+    for row in scraped_data:
+        print(row)
