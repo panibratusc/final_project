aeaaf9ed5d2a296b7953148e579169b0b3a5a507
Roman Panibratyuk <panibrat@usc.edu>
2024-04-11 12:41:24 -0700

    Headlines and Descriptions for NBC World sucessfully retrieved

--- src/get_data_2.py
+++ src/get_data_2.py
@@ -11,4 +11,10 @@ url = 'https://www.nbcnews.com/world'
 response = requests.get(url)
 if response.status_code == 200:
     html = response.text
-    print(html)
+    soup = BeautifulSoup(html, 'html.parser')
+    headlines_NBC = soup.find_all('h2', class_='wide-tease-item__headline')
+    descriptions_NBC = soup.find_all('div', class_='wide-tease-item__description')
+    for headline_NBC in headlines_NBC:
+        print(headline_NBC.text)
+    for description_NBC in descriptions_NBC:
+        print(description_NBC.text)
