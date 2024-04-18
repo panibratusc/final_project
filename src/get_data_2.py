292e4d8cbe199e1f61c4b21c70772d0d8519f1d3
Roman Panibratyuk <panibrat@usc.edu>
2024-04-18 10:23:32 -0700

    Files are successfully combined and saved in csv format

--- src/get_data_2.py
+++ src/get_data_2.py
@@ -4,6 +4,7 @@ import csv
 from bs4 import BeautifulSoup
 from io import StringIO
 import argparse
+import sys
 
 
 #Making the arrays the same length 
@@ -55,12 +56,13 @@ def get_CNN():
         html = response.text
         soup = BeautifulSoup(html, 'html.parser')
         headlines_CNN = soup.find_all('span', class_='container__headline-text')
+        headlines = [headline.text.strip() for headline in headlines_CNN]
         # descriptions_CNN = [[''] * len(headlines_CNN)]
         # headlines_CNN, descriptions_CNN = arrays_same_length(
         #     [headline.text for headline in headlines_CNN],
         #     [description.text for description in descriptions_CNN]
         # )
-        return headlines_CNN
+        return headlines
     else:
         print("Error: Response code", response.status_code)
         return []
@@ -68,16 +70,18 @@ def get_CNN():
 
 
 #Creating a datatable for all the news sources
-def create_data_table(headlines, descriptions):
+def create_data_table(headlines, descriptions,source):
     df = pd.DataFrame({
         'Headlines': headlines, 
-        'Descriptions': descriptions
+        'Descriptions': descriptions,
+        'Source': source
     })
     return df
 def main():
     parser = argparse.ArgumentParser(description='create a data table of descriptions and headlines from the various news sources')
     parser.add_argument('--source', nargs = '+', choices=['CNN','NYT','NBC'], help="Specify the news source: 'NBC', 'NYT', or 'CNN'", required=True)
-    parser.add_argument('--scrape', type=int, help="Specify the news source: 'NBC', 'NYT', or 'CNN'")
+    parser.add_argument('--scrape', type=int, help="Specify the number of records")
+    parser.add_argument('--save', type=argparse.FileType('w'), help="Specify the save location")
     args = parser.parse_args()
     data_tables = []
     for source in args.source:
@@ -88,11 +92,30 @@ def main():
         elif source == 'CNN':
             headlines = get_CNN()
             descriptions = [''] * len(headlines)
-        data_table = create_data_table(headlines, descriptions)
+        data_table = create_data_table(headlines, descriptions, source)
         data_tables.append(data_table)
         
     combined_data_table = pd.concat(data_tables, ignore_index=True)
-    print(combined_data_table)
+    if args.save:
+        combined_data_table.to_csv(args.save, index=False)
+        # fieldnames = ["sources", "descriptions", "headlines"]
+        # writer = csv.DictWriter(args.save, fieldnames=fieldnames)
+        
+        # # Write the header row
+        # writer.writeheader()
+        
+        # # Write the data rows
+        # for row in data_tables:
+        #     writer.writerow(row)
+        
+        # # Close the file explicitly
+        # args.save.close()
+        
+        print(f"Data has been saved to {args.save.name} in CSV format")
+
+        # pass
+    else:
+        print(combined_data_table)
     
 if __name__ == '__main__':
      main()
