dae3c72ed9afaec860ef367be82cbc1c28e4edeb
Roman Panibratyuk <panibrat@usc.edu>
2024-04-23 18:09:49 -0700

    Retriving NYT Data but doesn't work

--- src/final_version_code.py
+++ src/final_version_code.py
@@ -65,13 +65,14 @@ def get_NBC_Ukraine(limit=None):
     
 #results = get_NBC_Ukraine()
 
+#NYT Israel
 def get_author_and_date_NYT(url_series):
     result = []
     for url in url_series:
         page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
-        article_date = soup.select('div[class_ = "css-3xqm5e"] time')[0].contents[0]
-        article_author = soup.select('span.last-byline')[0].text
+        article_date = soup.select('div[class_ = "css-agrsgss"] time')[0].contents[0]
+        article_author = soup.select('div[class_ = "css-1i4y2t3]')[0].text
         result.append((article_author, article_date))
     return result
 
@@ -83,9 +84,11 @@ def get_NYT_IsraelPalestine(limit=None):
         soup = BeautifulSoup(html, 'html.parser')
         df_NYT_IsraelPalestine = pd.DataFrame(columns=['headline', 'description', 'url'])
         article_wrappers = soup.find_all('article', class_='css-1l4spti')
-        headlines_NYTIsraelPalestine = [article.find('h2', class_='css-1l8buln') for article in article_wrappers]
-        df_NYT_IsraelPalestine['headline'] = [headline_NYT.text for headline_NYT in headlines_NYTIsraelPalestine]
-        df_NYT_IsraelPalestine['description'] = [article.find('p', class_='css-1n8orw4').text for article in article_wrappers]
+        headlines_NYTIsraelPalestine = [article.find('h3', class_='css-1kv6qi') for article in article_wrappers]
+        df_NYT_IsraelPalestine['headline'] = [headline_NYT.text if headline_NYT else None for headline_NYT in headlines_NYTIsraelPalestine]
+        #df_NYT_IsraelPalestine['headline'] = [headline_NYT.text for headline_NYT in headlines_NYTIsraelPalestine]
+        df_NYT_IsraelPalestine['description'] = [article.find('p', class_='css-1n8orw4').text if article.find('p', class_='css-1n8orw4') else None for article in article_wrappers]
+        #df_NYT_IsraelPalestine['description'] = [article.find('p', class_='css-1n8orw4').text for article in article_wrappers]
         df_NYT_IsraelPalestine['url'] = [headline.parent['href'] for headline in headlines_NYTIsraelPalestine]
         authors_and_dates = get_author_and_date(df_NYT_IsraelPalestine['url'])
         df_NYT_IsraelPalestine['author'] = [author_and_date[0] for author_and_date in authors_and_dates]
@@ -100,3 +103,33 @@ def get_NYT_IsraelPalestine(limit=None):
         print("Error: Response code", response.status_code)
 
 get_NYT_IsraelPalestine()
+
+#NYT Ukraine_Russia
+def get_NYT_UkraineRussia(limit=None):
+    url = 'https://www.nytimes.com/news-event/ukraine-russia'
+    response = requests.get(url)
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        df_NYT_Ukraine_Russia = pd.DataFrame(columns=['headline', 'description', 'url'])
+        article_wrappers = soup.find_all('article', class_='css-1l4spti')
+        headlines_NYTUkraineRussia = [article.find('h3', class_='css-1kv6qi') for article in article_wrappers]
+        df_NYT_Ukraine_Russia['headline'] = [headline_NYT.text if headline_NYT else None for headline_NYT in headlines_NYTUkraineRussia]
+        #df_NYT_IsraelPalestine['headline'] = [headline_NYT.text for headline_NYT in headlines_NYTIsraelPalestine]
+        df_NYT_Ukraine_Russia['description'] = [article.find('p', class_='css-1n8orw4').text if article.find('p', class_='css-1n8orw4') else None for article in article_wrappers]
+        #df_NYT_IsraelPalestine['description'] = [article.find('p', class_='css-1n8orw4').text for article in article_wrappers]
+        df_NYT_Ukraine_Russia['url'] = [headline.parent['href'] for headline in headlines_NYTUkraineRussia]
+        authors_and_dates = get_author_and_date(df_NYT_Ukraine_Russia['url'])
+        df_NYT_Ukraine_Russia['author'] = [author_and_date[0] for author_and_date in authors_and_dates]
+        df_NYT_Ukraine_Russia['date'] = [author_and_date[1] for author_and_date in authors_and_dates]
+        
+        if limit:
+            df_NYT_Ukraine_Russia = df_NYT_Ukraine_Russia.head(limit)
+        print(df_NYT_Ukraine_Russia)
+        return df_NYT_Ukraine_Russia
+        
+    else:
+        print("Error: Response code", response.status_code)
+
+get_NYT_UkraineRussia()
+
